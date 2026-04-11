92537d4b3c486d24e9ffebca1c3f407d25c0ef03
smrithimala7-project <smrithimala7@gmail.com>
2026-04-11 18:24:04 +0530

    Update Q01.py

--- Q01.py
+++ Q01.py
@@ -13,4 +13,17 @@
 # Sample Input:   Enter your score: 85
 # Sample Output:  Grade: B
 
-# --- YOUR CODE HERE ---
+score=int(input("Enter your score:"))
+if(score<0 or score>100):
+  print("Invalid score")
+else:
+  if(score>=90 and score<=100):
+    print("Grade: A")
+  elif(score>=80 and score<90):
+    print("Grade: B")
+  elif(score>=70 and score<80):
+    print("Grade: C")
+  elif(score>=60 and score<70):
+    print("Grade: D")
+  else:
+    print("Grade: F")

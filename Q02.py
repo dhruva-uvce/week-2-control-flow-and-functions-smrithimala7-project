4f1d2620fa07ce915e63fcef75efd9b3c8b12825
smrithimala7-project <smrithimala7@gmail.com>
2026-04-12 12:27:25 +0530

    Update Q02.py

--- Q02.py
+++ Q02.py
@@ -14,4 +14,8 @@
 # Sample Input 2:   Enter a year: 1900
 # Sample Output 2:  1900 is not a leap year
 
-# --- YOUR CODE HERE ---
+year=int(input())
+if(year%400==0 or (year%100!=0 and year%4==0)):
+    print("leap year")
+else:
+    print("not leap year")

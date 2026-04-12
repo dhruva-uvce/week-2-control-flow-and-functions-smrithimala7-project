4830917bc09df19a06acf75b00f3f7836af1f42a
smrithimala7-project <smrithimala7@gmail.com>
2026-04-12 12:26:28 +0530

    Update Q01.py

--- Q01.py
+++ Q01.py
@@ -14,16 +14,16 @@
 # Sample Output:  Grade: B
 
 score=int(input())
-if(score<0 or score>100):
-  print("invalid")
+if(score>100 or score<0):
+    print("invalid")
 else:
-  if(score>=90 and score<=100):
-    print("A")
-  elif(score>=80 and score<90):
-    print("B")
-  elif(score>=70 and score<80):
-    print("C")
-  elif(score>=60 and score<70):
-    print("D")
-  else:
-    print("F")
+    if(score>=90 and score<=100):
+        print("A")
+    elif(score>=80 and score<90):
+        print("B")
+    elif(score>=70 and score<80):
+        print("C")
+    elif(score>=60 and score<70):
+        print("D")
+    else:
+        print("F")
